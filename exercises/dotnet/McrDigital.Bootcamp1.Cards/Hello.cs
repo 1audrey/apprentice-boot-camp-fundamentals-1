d301b21f128f45aa1d30a21fc67fd01d3a7a9f10
Megan McClellan <Megan_McClellan@waters.com>
2021-03-15 11:02:22 +0000

    Added HelloWorld

---
+++ exercises/dotnet/McrDigital.Bootcamp1.Cards/Hello.cs
@@ -0,0 +1,13 @@
+namespace McrDigital.Bootcamp1.Cards
+{
+    using System;
+
+    public class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+
+    }
+}
